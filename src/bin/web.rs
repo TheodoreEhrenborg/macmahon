3c2b802eb5c9dff722dc77ab6df20ae2ea5468f0
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-04-17 13:25:16 +0100

    Make a text box

--- src/bin/web.rs
+++ src/bin/web.rs
@@ -1,12 +1,28 @@
+use web_sys::HtmlTextAreaElement;
 use yew::prelude::*;
 
-#[function_component(App)]
-fn app() -> Html {
+#[function_component(TextEditor)]
+fn text_editor() -> Html {
+    // Create state for the textarea content
+    let value = use_state(|| "I am writing a long story...".to_string());
+
+    // Callback for handling text input changes
+    let on_text_input = {
+        let value = value.clone();
+
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlTextAreaElement = e.target_unchecked_into::<HtmlTextAreaElement>();
+            value.set(input.value());
+        })
+    };
+
     html! {
-        <h1>{ "Hello World" }</h1>
+        <div class="content-area">
+            <textarea value={value.to_string()} oninput={on_text_input} />
+        </div>
     }
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<TextEditor>::new().render();
 }
